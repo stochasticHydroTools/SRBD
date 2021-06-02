afae66884319d4c1f830c6228fb79008602831ba
Kishore Basu <basu@tara.astro.uwo.ca>
2021-06-02 14:23:31 -0400

    This file is for the diffusion of a simple, spring-less particle. The goal is to write out the data to a file, with (in this case) 100 data points, for each integer multiple tau. Have not been able to run yet because of BoxLibRNG issue

---
+++ DimerTests/COM_OneParticle.f90
@@ -0,0 +1,51 @@
+! File to diffuse and gather data on ONE particle
+program test
+    use MinHeapModule
+    use BoxLibRNGs ! Random number generation
+    implicit none
+
+    real :: tau = 1.0/(0.1)
+    real, dimension(3) :: r = 0.0
+    real, allocatable :: pos(:,:)
+    integer :: n , i
+
+    n = 100 ! Number of data entries (not including the starting point)
+    allocate(pos(3,n + 1))
+    pos(:,1) = r
+
+    do i = 1, n
+        call brownianStep(r, pos, i, tau)
+    end do
+
+    ! Write to File
+    call writeToFile(pos)
+
+    deallocate(pos)
+
+    contains
+
+        ! Brownian Step
+        subroutine brownianStep(r, pos, i, tau)
+            integer i 
+            real tau, disp
+            real, dimension(3) :: r 
+            real, dimension(3,n+1) :: pos
+            real :: D = 9 ! Diffusion coefficient of species A
+
+            call NormalRNGVec(numbers=disp, n_numbers=3) ! Mean zero and variance one
+            disp = sqrt(2*D*tau)*disp ! Make the variance be 2*D*time
+            pos(:,i + 1) = pos(:,i) + disp
+
+        end subroutine
+        
+        ! Write to File
+        subroutine writeToFile(pos)
+            real, dimension(3,n+1) :: pos
+
+            open(7,file = 'output.txt')
+            write(7,*) pos
+            close(7)
+
+        end subroutine
+        
+end program

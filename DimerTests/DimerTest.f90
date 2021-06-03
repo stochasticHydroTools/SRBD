94b8d722e672ee8f6c9172d11d6e93c5f329552b
Kishore Basu <basu@tara.astro.uwo.ca>
2021-06-02 20:03:31 -0400

    Euler-Mauryama method. Haven't yet checked fully, as BoxLibRNGs are inaccesible. Meant to be done on r1 (that is, we are diffusing r1)
    :q
    :wq

--- DimerTests/DimerTest.f90
+++ DimerTests/DimerTest.f90
@@ -5,23 +5,23 @@ program EM
     real, dimension(3) :: r1, r2, x
     r1 = (/0.0, 0.0, 0.0/)
     r2 = (/0.0, 0.0, 0.5/)
-    x = (/1.0, 1.0, 1.0/)
 
-    call Euler_Maruyama(0.05, x, 100, 1.0, 3.0, 5.0, 1.0, r1, r2)
+    call Euler_Maruyama(0.05, 100, 1.0, 3.0, 5.0, 1.0, r1, r2)
 
     contains 
-        subroutine Euler_Maruyama(dt,x,nsteps,a,visc,k,l0,r1,r2)
+
+        ! r1 is the position of the bead which we are diffusing by one time step. 
+        subroutine Euler_Maruyama(dt,nsteps,a,visc,k,l0,r1,r2)
             real :: dt, disp, a, visc, k, l0, mu, l12
             integer :: nsteps
             real, parameter :: KB = 1.38065*(10**(-23)), T = 300, pi = 4.D0*DATAN(1.D0)
-            real, dimension(3), intent(in) :: r1, r2
-            real, dimension(3) :: x
+            real, dimension(3) :: r1, r2
         
             ! Local Variables
             integer :: i
 
             ! Initialization of constants
-            mu = ( 1 / (6*pi*visc*a) ) 
+            mu = ( 1.0 / (6*pi*visc*a) ) 
             l12 = NORM2(r1-r2)
                     
             ! Brownian motion (only) realization (code could be modified to be in R3)
@@ -35,7 +35,7 @@ program EM
             ! Brownian Motion with Deterministic Drift Realization
             do i = 1, nsteps
                 call NormalRNGVec(numbers=disp, n_numbers= 3) ! Mean zero and variance one
-                x = x + (mu * k * (l12 - l0) * (r1-r2) / l12) * dt + sqrt(2*KB*T*dt)*disp ! Apply one Euler-Maruyama Step        
+                r1 = r1 + (mu * k * (l12 - l0) * (r1-r2) / l12) * dt + sqrt(2*KB*T*dt)*disp ! Apply one Euler-Maruyama Step        
             end do   
 
         end subroutine

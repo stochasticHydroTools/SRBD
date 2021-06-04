246a48b04d1f6c845a58d96d525842dfcd640227
Kishore Basu <kb4063@access2.cims.nyu.edu>
2021-06-04 15:05:47 -0400

    Preliminary stuff on the COM spring calculation

---
+++ DimerTests/COM_TwoParticlesSpring.f90
@@ -0,0 +1,104 @@
+program ParticleSpring
+    use Precision
+    use BoxLibRNGs
+    implicit none 
+
+    integer, parameter                  :: wp = r_sp
+    real(wp), parameter                 :: k = 1.0_wp, l0 = 1.0_wp, a = 1.0_wp, pi = 4.0_wp*ATAN(1.0_wp), visc = 1.0_wp
+    real(wp), dimension(3)              :: r1, r2, rcm
+    integer n, i
+    real(wp), allocatable               :: pos1(:,:), pos2(:,:), pos_cm(:,:)
+    real(wp)                            :: tau
+
+    tau = k * 1.0_wp/(6*pi*visc*a)
+
+    n = 100
+
+    ! Initialize starting positions.
+    r1 = 0.0
+    r2 = 1.0
+    rcm = 0.5 * (r1 + r2)
+
+    ! Allocate Memory for all vectors
+    allocate(pos1(3,n + 1))
+    allocate(pos2(3,n + 1))
+    allocate(pos_cm(3,n + 1))
+
+    ! We will continue to update these position arrays and then will print out only the COM one. The others are for tracking data
+    pos1(:,1) = r1
+    pos2(:,1) = r2
+    pos_cm(:,1) = rcm
+    
+    do i = 1, n
+        ! Apply one diffusive step to both r1 and r2 simultaneously.
+        call Euler_Maruyama(tau, 1, a, visc, k, l0, r1, r2)        ! Just one tau step each iterate.
+        pos1(:, i + 1) = r1
+        pos2(:, i + 1) = r2
+        pos_cm(:, i + 1) = 0.5 * (pos1(:,i+1) + pos2(:, i + 1))
+
+    end do
+
+    call writeToFile(pos_cm, n)
+
+    deallocate(pos1)
+    deallocate(pos2)
+    deallocate(pos_cm)
+        
+
+
+    contains
+
+        ! r1 is the position of the bead which we are diffusing by one time step. 
+        subroutine Euler_Maruyama(dt,nsteps,a,visc,k,l0,r1,r2)
+            implicit none
+            real(wp), intent(in) :: dt, a, visc, k, l0
+            integer, intent(in) :: nsteps
+            real(wp), dimension(3), intent(inout) :: r1, r2
+            real(wp), dimension(3) :: disp1, disp2, temp
+            real(wp), parameter :: KB = 1.38065E-23_wp, T = 300.0_wp, pi = 4.0_wp*ATAN(1.0_wp)
+            real(wp) mu, l12
+
+            ! Local Variables
+            integer :: i
+
+            ! Initialization of constants
+            mu = ( 1.0 / (6*pi*visc*a) ) 
+            l12 = NORM2(r1-r2)
+
+            ! Brownian Motion with Deterministic Drift Realization
+            do i = 1, nsteps
+                call NormalRNGVec(numbers=disp1, n_numbers= 3) ! Mean zero and variance one
+                call NormalRNGVec(numbers=disp2, n_numbers= 3) ! Mean zero and variance one
+
+                temp = r1 - r2
+
+                r1 = r1 + (mu * k * (l12 - l0) * (temp) / l12) * dt + sqrt(2*KB*T*mu*dt)*disp1 ! Apply one Euler-Maruyama Step   
+                r2 = r2 + (mu * k * (l12 - l0) * (-temp) / l12) * dt + sqrt(2*KB*T*mu*dt)*disp2
+
+            end do   
+
+        end subroutine      
+
+
+          ! Write to File
+        subroutine writeToFile(pos,n)
+            integer, intent(in) :: n
+            real(wp), dimension(3,n+1), intent(in) :: pos
+            integer i, j
+            
+
+            open(unit=2, file='data3.txt', ACTION="write", STATUS="replace")
+            do i=1,3
+                write(2, '(1000F14.7)')( real(pos(i,j)) ,j=1,n+1)
+            end do
+        
+            close(2)
+        
+
+        end subroutine
+        
+    
+
+
+
+end program

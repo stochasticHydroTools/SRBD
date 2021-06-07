199fbe5f7813fe7b9252fdca3dfc9429dc431401
Kishore Basu <kb4063@spde.cims.nyu.edu>
2021-06-07 10:36:30 -0400

    Made this work for BOTH r1 and r2 in one time step.

--- DimerTests/DimerTest.f90
+++ DimerTests/DimerTest.f90
@@ -22,30 +22,30 @@ program EM
             real(wp), intent(in) :: dt, a, visc, k, l0
             integer, intent(in) :: nsteps
             real(wp), dimension(3), intent(inout) :: r1, r2
-            real(wp), dimension(3) :: disp
+            real(wp), dimension(3) :: disp1, disp2, temp
             real(wp), parameter :: KB = 1.38065E-23_wp, T = 300.0_wp, pi = 4.0_wp*ATAN(1.0_wp)
             real(wp) mu, l12
+
 
             ! Local Variables
             integer :: i
 
             ! Initialization of constants
             mu = ( 1.0 / (6*pi*visc*a) ) 
-                    
-            ! Brownian motion (only) realization (code could be modified to be in R3)
-            ! do i = 1, nsteps
-            !    call NormalRNG(number = disp) ! Mean zero and variance one
-            !
-                ! Apply one Euler-Maruyama step
-            !    x = x + sqrt(2*KB*T*dt)*disp
-            !end do   
 
             ! Brownian Motion with Deterministic Drift Realization
             do i = 1, nsteps
-		l12 = NORM2(r1-r2)
-                call NormalRNGVec(numbers=disp, n_numbers= 3) ! Mean zero and variance one
-                r1 = r1 + (mu * k * (l12 - l0) * (r1-r2) / l12) * dt + sqrt(2*KB*T*mu*dt)*disp ! Apply one Euler-Maruyama Step        
-            end do   
+                call NormalRNGVec(numbers=disp1, n_numbers= 3) ! Mean zero and variance one
+                call NormalRNGVec(numbers=disp2, n_numbers= 3) ! Mean zero and variance one
+
+                l12 = norm2(r1-r2)
+
+                temp = r1 - r2
+
+                r1 = r1 + (mu * k * (l12 - l0) * (-temp) / l12) * dt + sqrt(2*KB*T*mu*dt)*disp1 ! Apply one Euler-Maruyama Step   
+                r2 = r2 + (mu * k * (l12 - l0) * (temp) / l12) * dt + sqrt(2*KB*T*mu*dt)*disp2
+
+            end do
 
         end subroutine      
  

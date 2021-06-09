1ac7d6ff7c5b5be9898a871bd0d1894ab78043ed
Kishore Basu <kb4063@spde.cims.nyu.edu>
2021-06-09 15:52:36 -0400

    Fixed code with Donev comments from Spring case

--- DimerTests/COM_OneParticle.f90
+++ DimerTests/COM_OneParticle.f90
@@ -1,39 +1,38 @@
 ! File to diffuse and gather data on ONE particle
 program OneParticle
     use Precision
-    use BoxLibRNGs ! Random number generation
-    implicit none
+    use BoxLibRNGs
+    use, intrinsic :: iso_fortran_env, only: stderr => error_unit       ! This is so I can track errors.
+    implicit none 
 
-    integer, parameter      :: wp = r_sp, dim = 3
-    real(wp), parameter     :: dt = 0.001_wp, D = 9.0_wp
-    real(wp), dimension(dim)  :: r = 0.0_wp
-    real(wp), allocatable   :: pos(:,:)
-    integer                 :: n , i
-    character(15)           :: filename = 'data1.txt'
+    integer, parameter          :: wp = r_sp, dim = 3
+    real(wp)                    :: dt, D
+    real(wp), dimension(dim)    :: r = 0.0_wp
+    real(wp), allocatable       :: pos(:,:)
+    integer                     :: n , i, unit1
+    character(len = 128)        :: filename = 'data1.txt', nml_file = 'brownianParticle.nml'
 
-    n = 10000 ! Number of data entries (not including the starting point) -- will be large
+    call read_namelist(nml_file, dt, D, n)
 
-    allocate(pos(dim,n + 1))
-    pos(:,1) = r
+    open(newunit = unit1, file = filename)
 
     do i = 1, n
-        call brownianStep(r, i, dt,dim, D)
-        pos(:,i+1) = r
+        write(unit1,*) r(:)
+        call brownianStep(r, dt,dim, D)
     end do
 
-    ! Write to File
-    call writeToFile(pos,n,dim, filename)
+    write(unit1,*) r(:)
 
-    deallocate(pos)
+    close(unit1)
 
     contains
 
         ! Brownian Step
-        subroutine brownianStep(r, i, dt,dim, D)
-            integer , intent(in)                            :: i, dim
+        subroutine brownianStep(r, dt,dim, D)
+            integer , intent(in)                            :: dim
             real(wp), intent(in)                            :: dt, D
             real(wp), dimension(dim)                        :: disp
-            real(wp), dimension(dim), intent(inout)     :: r
+            real(wp), dimension(dim), intent(inout)         :: r
 
             call NormalRNGVec(numbers=disp, n_numbers=dim) ! Mean zero and variance one
 
@@ -43,21 +42,36 @@ program OneParticle
 
         end subroutine
         
-        ! Write to File
-        subroutine writeToFile(pos,n,dim, filename)
-            integer, intent(in)                     :: n, dim
-            real(wp), dimension(3,n+1), intent(in)  :: pos
-            character(15)                           :: filename
-            integer j, i
-
-            open(12, file = filename)
-            do j = 1, n + 1
-                write(12, *) (pos(i,j), i = 1, dim)
-            end do
-
-            close(12)
-        
-
-        end subroutine
+        ! Subroutine to read in the data from a namelist file. 
+        subroutine read_namelist(file_path, dt, D, n)
+            ! Reads Namelist from given file.
+            character(len=*),  intent(in)  :: file_path
+            integer,           intent(out) :: n
+            real,              intent(out) :: dt, D
+            integer                        :: unit, check
+    
+            ! Namelist definition.
+            namelist /brownianParticle/ dt, D, n
+    
+            ! Check whether file exists.
+            inquire (file=file_path, iostat=check)
+    
+            ! Here we have some checks, this just makes sure that the file is around. 
+            if (check /= 0) then
+                write (stderr, '(3a)') 'Error: The file "', trim(file_path), '" does not exist.'
+                return
+            end if
+    
+            ! Open and read Namelist file.
+            open (action='read', file=file_path, iostat=check, newunit=unit)
+            read (nml=brownianParticle, iostat=check, unit=unit)
+    
+            ! This is to keep people aware of cases like : End of File runtime errors.
+            if (check /= 0) then
+                write (stderr, '(a)') 'Error: invalid Namelist format.'
+            end if
+    
+            close (unit)
+        end subroutine read_namelist
         
 end program

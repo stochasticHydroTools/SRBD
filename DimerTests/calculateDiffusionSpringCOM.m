be2e3fd5798634362dbba31f2ab7da977bde1d1d
Kishore Basu <kb4063@spde.cims.nyu.edu>
2021-06-15 15:35:36 -0400

    CI are wrong, but have added some changes to bin count.

--- DimerTests/calculateDiffusionSpringCOM.m
+++ DimerTests/calculateDiffusionSpringCOM.m
@@ -7,21 +7,22 @@ format long
 
 d = 1;                          %Dimension
 mu = 1.0;
-k = 0.029;
+k = 1.0;
 l0 = 0;
 
 A = importdata('diffusion.txt')';
 tau = 1 / (k* mu);        
 jmax = 100;
-Diffusion = zeros(jmax,1);   
+Diffusion = zeros(jmax,1); 
+temp = zeros(jmax,1);
 
 for j = 1:jmax
     sum = 0;
     for p = 1:length(A) - j
        sum = sum + norm(A(:,p+j) - A(:,p))^2;
     end
-    Diffusion(j) = sum ./ (length(A) - j);
-    Diffusion(j) = Diffusion(j) ./ (2*d*j*tau);  
+    temp(j) = sum ./ (length(A) - j);
+    Diffusion(j) = temp(j) ./ (2*d*j*tau);  
 end
 
 B = importdata('relDist.txt');
@@ -45,12 +46,12 @@ disp(1 / 2)
 figure(1)
 %If 1D, histogram B, if 3D, histogram C (this corresponds to displacement
 %vs distance)
-h = histogram(B(1000:end),'Normalization','pdf'); 
+h = histogram(B(1000:end),'Normalization','pdf','Binwidth',0.1); 
 xlabel('Distance between particles r1, r2')
 ylabel('Probability')
 
 hold on
-x = linspace(-20,20,10001);
+x = linspace(-5,5,10001);
 y = (1/(sqrt(2*pi*1/k)))*exp((-k*(x-l0).^2)./ (2*1));  %Will be the boltzmann distribution ( D = 1)
 plot(x,y,'Linewidth',2)
 
@@ -65,6 +66,13 @@ CI = mean(B) + ts*SE;
 xline(CI(1), 'Linewidth',1.25)
 xline(CI(2),'Linewidth',1.25)
 
+figure(2)
+t = 1:1:jmax;
+t = t*tau;
+
+plot(t,temp,'o','Linewidth',2)
+xlabel('n*dt')
+ylabel('<r_{cm}^2>')
 
 
  

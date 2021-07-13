16573c1b73bc7c5ae4de7509559471b78404c029
Kishore Basu <kb4063@spde.cims.nyu.edu>
2021-07-13 17:16:40 -0400

    Same changes as in upper folder

--- DimerTests/COM_TwoParticlesSpring.f90
+++ DimerTests/COM_TwoParticlesSpring.f90
@@ -9,69 +9,90 @@ program ParticleSpring
     integer, parameter                                  :: wp = r_sp, dim = 3
     !real(wp), parameter                                :: pi = 4.0_wp*ATAN(1.0_wp), KB = 1.38065E-23_wp, T = 300.0_wp
     real(wp), dimension(dim)                            :: r1, r2, r_cm, r_rel
-    integer                                             :: i, myunit3, myunit4, n, nsteps
-    character(len=128)                                  :: filenameDiff = 'diffusion.txt', filenameRel = 'relDist.txt'
-    logical, parameter                                  :: rotational = .false.
+    integer                                             :: i, myunit3, myunit4, n, nsteps, numIterates, m
+    character(len=128)                                  :: filenameDiff = 'diffusion.txt', filenameRel = 'relDistTest.txt'
+    logical, parameter                                  :: rotational = .false., timeScaleSpring = .true., confidenceInterval = .false.
     integer                                             :: seed = 5
-    real(wp)                                            :: mu_1, mu_2, tau, dimensionlessTSSize, dt
+    real(wp)                                            :: mu_1, mu_2, dimensionlessTSSize, dt
+    real(wp), parameter                                 :: pi = 4.0_wp*ATAN(1.0_wp)
+    character(len = 128), dimension(16)                 :: seedFilenames
 
+    call readCLParameters()
     call initializeCLs()
 
+    n = 100000         ! How many data points saved (this is not too related to DiffusionCLs, so am putting it here) 
+    dimensionlessTSSize = 1.5_wp   
+    nsteps = 1
+    numIterates = 1
 
-    n = 500000          ! How many data points saved (this is not too related to DiffusionCLs, so am putting it here) 
-    dimensionlessTSSize = 0.01   
-    nsteps = 100 
+    seedFilenames = (/'diffusionCSeed05.txt','diffusionCSeed06.txt','diffusionCSeed07.txt', &
+    'diffusionCSeed08.txt', 'diffusionCSeed09.txt', 'diffusionCSeed10.txt', &
+    'diffusionCSeed11.txt', 'diffusionCSeed12.txt','diffusionCSeed13.txt', &
+    'diffusionCSeed14.txt', 'diffusionCSeed15.txt', 'diffusionCSeed16.txt', &
+    'diffusionCSeed17.txt', 'diffusionCSeed18.txt', 'diffusionCSeed19.txt', &
+    'diffusionCSeed20.txt' /)
+
+    !seedFilenames = (/'rotationalTimeScale_k_5.0_dtau_s_0.1Seed05.txt','rotationalTimeScale_k_5.0_dtau_s_0.1Seed06.txt','rotationalTimeScale_k_5.0_dtau_s_0.1Seed07.txt', &
+    !'rotationalTimeScale_k_5.0_dtau_s_0.1Seed08.txt', 'rotationalTimeScale_k_5.0_dtau_s_0.1Seed09.txt', 'rotationalTimeScale_k_5.0_dtau_s_0.1Seed10.txt', &
+    !'rotationalTimeScale_k_5.0_dtau_s_0.1Seed11.txt', 'rotationalTimeScale_k_5.0_dtau_s_0.1Seed12.txt','rotationalTimeScale_k_5.0_dtau_s_0.1Seed13.txt', &
+    !'rotationalTimeScale_k_5.0_dtau_s_0.1Seed14.txt', 'rotationalTimeScale_k_5.0_dtau_s_0.1Seed15.txt', 'rotationalTimeScale_k_5.0_dtau_s_0.1Seed16.txt', &
+    !'rotationalTimeScale_k_5.0_dtau_s_0.1Seed17.txt', 'rotationalTimeScale_k_5.0_dtau_s_0.1Seed18.txt', 'rotationalTimeScale_k_5.0_dtau_s_0.1Seed19.txt', &
+    !'rotationalTimeScale_k_5.0_dtau_s_0.1Seed20.txt' /)
+
+    if (confidenceInterval) numIterates = 16
+
+    do m = 0, numIterates - 1
+        seed = seed + m
+        call SeedRNG(seed) 
+
+        ! Initialize starting positions.
+        r1 = 1.0_wp
+        r2 = -1.0_wp 
+    
+        if(confidenceInterval) filenameDiff = seedFilenames(m + 1)
 
 
-    call SeedRNG(seed) 
+        open(newunit = myunit3, file = filenameDiff)
+        open(newunit = myunit4, file = filenameRel) 
 
-    ! Initialize starting positions.
-    r1 = -1.0_wp
-    r2 = 1.0_wp 
-  
-    open(newunit = myunit3, file = filenameDiff)
-    open(newunit = myunit4, file = filenameRel) 
+        mu_1 = mu_1_0
+        mu_2 = mu_2_0
 
-    mu_1 = 1.0_wp
-    mu_2 = 1.0_wp
-
-    tau = 1 / (k_s * (mu_1 + mu_2))
-    dt = dimensionlessTSSize * tau
-
-
-    r_cm = mu_2 * r1 / (mu_1 + mu_2) + mu_1 * r2 / (mu_1 + mu_2)
-    r_rel = r1-r2  
-
-         
-    do i = 0, n
-
-        if (evolve_r_cm) write(myunit3,*) r_cm 
-        
-        if (rotational) then
-            write(myunit4,*) r_rel / norm2(r_rel)     
-
+        if (timeScaleSpring) then 
+            dt = dimensionlessTSSize * tau_s        ! For spring time scale
         else
-            write(myunit4,*) r_rel       
+            dt = dimensionlessTSSize * tau_r        ! For rotational time scale
         end if
+    
+        r_cm = mu_2 * r1 / (mu_1 + mu_2) + mu_1 * r2 / (mu_1 + mu_2)
+        r_rel = r1-r2  
 
-        select case (sde_integrator_enum)
+            
+        do i = 0, n
 
-        ! For now, will not use the exact sol since this is only useful for l0 = 0
-        case(1)
-            call eulerMaruyama(dt, nsteps, mu_1, mu_2, r_cm, r_rel)
+            if (evolve_r_cm) write(myunit3,*) r_cm 
+            
+            if (rotational) then
+                write(myunit4,*) r_rel / norm2(r_rel)     
 
-        case(2)
-            call explicitMidpoint(dt, nsteps, mu_1, mu_2, r_cm, r_rel)
+            else
+                write(myunit4,*) r_rel       
+            end if
 
-        case(3)
-            call implicitTrapezoidal(dt, nsteps, mu_1, mu_2, r_cm, r_rel)
 
-        end select
-        
-    end do
+            r1 = r_rel + (r_cm * (mu_1 + mu_2) - mu_2 * r_rel) / (mu_1 + mu_2)
+            r2 = (r_cm * (mu_1 + mu_2) - mu_2 * r_rel) / (mu_1 + mu_2)
 
-    close(myunit3)
-    close(myunit4)       
+            call moveDimer(dt, nsteps, mu_1, mu_2, r1, r2)
+
+            r_cm = mu_2 * r1 / (mu_1 + mu_2) + mu_1 * r2 / (mu_1 + mu_2)
+            r_rel = r1-r2  
+            
+        end do
+
+        close(myunit3)
+        close(myunit4)   
+    end do    
     
     contains
 
@@ -79,9 +100,9 @@ program ParticleSpring
         ! This is detailed in the Ornstein-Uhlenbeck wikipedia page, and it states that
         ! r_d = r0 exp( -theta * t) + sigma / sqrt(2*theta)exp(-theta*t)W*
         ! Where W* is a Wiener increment with variance exp(2*theta*t) - 1, and also theta = - mu_eff *k * (l0-l12)/l12
-        subroutine exactSol(dt, nstepsCLs, mu_eff, k, D_rel, l0, r_rel)   
+        subroutine exactSol(dt, nsteps, mu_eff, k, D_rel, l0, r_rel)   
             real(wp), intent(in)                        :: dt, mu_eff, k, l0, D_rel
-            integer, intent(in)                         :: nstepsCLs
+            integer, intent(in)                         :: nsteps
             real(wp), dimension(dim), intent(inout)     :: r_rel
 
             ! Local Variables
@@ -89,7 +110,7 @@ program ParticleSpring
             real(wp)                                    :: l12, theta
             integer                                     :: i
 
-            do i = 1, nstepsCLs
+            do i = 1, nsteps
                 call NormalRNGVec(numbers = disp1, n_numbers = dim) ! Mean zero and variance one
 
 
--- DimerTests/DiffusionCLs.f90
+++ DimerTests/DiffusionCLs.f90
@@ -6,13 +6,14 @@ module DiffusionCLs
     save
 
     integer, parameter, private                     :: pr = r_sp, dim = 3       ! MUST BE CONSISTENT WITH MAIN CODE!!!!
-    real(pr)                                        :: kbT = 0.1_pr ! Boltzmann constant in units of micrometers
+    real(pr)                                        :: kbT = 4.0E-3_pr ! Boltzmann constant in units of pN * um 
     real(pr)                                        :: a_1=1.0_pr, a_2=1.0_pr, visc=1.0_pr, k_s=1.0_pr, l0=1.0_pr
     real(pr)                                        :: mu_1_0, mu_2_0, tau_s, tau_r ! Computed values
-    integer                                         :: sde_integrator_enum=3 != 1: Euler-Maruyama, 2: Explicit Midpoint, 3: Implicit Trapezoidal
+    integer                                         :: sde_integrator_enum=3, nsteps_CLs = 1 !sde_integrator_enum = 
+    !1 : Euler-Maruyama, 2: Explicit Midpoint, 3: Implicit Trapezoidal
     character(len = 128)                            :: nml_file = "diffCLs.nml" ! Can be changed to read namelist from different file
     logical                                         :: evolve_r_cm = .true.
-    real(pr), parameter, private                    :: pi = 4.0_pr*ATAN(1.0_pr)
+    real(pr), parameter, private                    :: pi = 4.0_pr*ATAN(1.0_pr) !1.0_pr/6.0_pr ! Donev: TEMPORARY, set 6*pi=1, 4.0_pr*ATAN(1.0_pr)
     
     ! These parameters are in this module since they are required for SRBD, however, they are not actually used in this module
     ! This is done so that minimal changes are made to DoiBoxModule and most changes are here
@@ -33,14 +34,22 @@ module DiffusionCLs
             ! Other things, like filenames to write to are not included, because this is oly needed in
             ! COM_TwoParticlesSpring.f90 and may not be used in all cases when I use this module.            
             mu_1_0 = 1.0_pr / (6 * pi * visc * a_1)
-            mu_2_0 = 1.0_pr / (6 * pi * visc * 0.02_pr)
+            mu_2_0 = 1.0_pr / (6 * pi * visc * a_2)
             
             tau_s = 1.0_pr / (k_s * (mu_1_0 + mu_2_0))       ! Spring time scale
             tau_r = l0 * l0 / (2 * kbT * (mu_1_0 + mu_2_0))  ! Rotational time scale
 
         end subroutine
-        
 
+        ! Assuming 3D. 
+        subroutine outputCLs(particle, specie, x, y, z)
+            real(pr), intent(in)                    :: x, y, z 
+            integer, intent(in)                     :: particle, specie
+
+            write(77,*) particle, specie, x, y, z 
+            
+        end subroutine
+        
         ! Subroutine to read in the data from a namelist file. 
         subroutine readCLParameters(nml_unit)
             integer,  intent(in), optional  :: nml_unit
@@ -48,7 +57,7 @@ module DiffusionCLs
 
             ! Namelist definition.
             namelist /diffCLs/ k_s, l0, a_1, a_2, visc, sde_integrator_enum, evolve_r_cm, &
-                               add_springs, n_dimers, n_fiber_blobs
+                               add_springs, n_dimers, n_fiber_blobs, nsteps_CLs
             
             if(present(nml_unit)) then
                unit=nml_unit
@@ -98,6 +107,8 @@ module DiffusionCLs
                call explicitMidpoint(dt, nsteps, mu_1, mu_2, r_cm, r_rel)
             case(3)
                call implicitTrapezoidal(dt, nsteps, mu_1, mu_2, r_cm, r_rel) 
+            case(4)
+                call seperateTimeScales(dt, nsteps, mu_1, mu_2, r_cm, r_rel)
             end select
 
             ! Use r_CM and r_rel to reconstruct the displacement of each cross-linker
@@ -247,6 +258,92 @@ module DiffusionCLs
             end do
 
         end subroutine                
+
+        ! Apply the implicit trapezoidal method to the dl equation and
+        ! the Euler-Lie integrator for rotation on the unit sphere, du
+        ! Probably best to think of a better subroutine name
+        subroutine seperateTimeScales(dt, nsteps, mu_1, mu_2, r_cm, r_rel)
+            real(pr), intent(in)                        :: dt, mu_1, mu_2
+            integer, intent(in)                         :: nsteps
+            real(pr), dimension(dim), intent(inout)     :: r_cm, r_rel
+
+            ! Local Variables
+            real(pr), dimension(dim)                    :: u, disp1, disp3, N_hat
+            real(pr)                                    :: L_n, mu_eff, D_rel, G, G_pred, theta, sdev_cm, D_cm
+            integer                                     :: i
+            real(pr), dimension(1)                      :: disp2, l, l_pred
+
+            ! Declare variables
+            mu_eff = mu_1 + mu_2 
+            D_rel = kbT * mu_eff
+            D_cm = kbT * mu_1 * mu_2 / (mu_eff)        ! Diffusive coeff for r_cm
+
+            ! Change coordinates
+            l = norm2(r_rel)
+            u = r_rel / l(1)
+
+            do i = 1, nsteps
+
+                ! Euler-Lie Integrator for du (must do this first bc uses l)
+                ! Rodrigues Rotation Formula
+                call NormalRNGVec(numbers = disp1, n_numbers = dim) ! Mean zero and variance one
+                ! Rotating angle
+                theta = sqrt(2 * D_rel * dt) * norm2(disp1) / l(1) 
+                
+                N_hat = disp1 / norm2(disp1)
+                call rotate(u, theta, N_hat)
+
+                ! Implicit Trapezoidal method for dl. 
+                L_n = - mu_eff * k_s 
+                G = 2 * kbT * mu_eff / l(1) + l0 * mu_eff * k_s ! Must be scalar so choose 1st component
+
+                ! Random variate for the scalar equation dl
+                call NormalRNGVec(numbers = disp2, n_numbers = 1) ! Always a scalar
+                disp2 = sqrt(2 * D_rel * dt) * disp2
+
+                ! Predictor
+                l_pred = l + 0.5_pr * dt * L_n * l + dt * G + disp2
+                l_pred = l_pred / (1 - dt * L_n / 2)
+
+                ! Corrector
+                G_pred = 2 * kbT * mu_eff / l_pred(1) + l0 * mu_eff * k_s
+                l = l + 0.5_pr * dt * (L_n * l) + 0.5_pr * dt * (G + G_pred) + disp2
+                l = l / (1 - dt * L_n / 2)
+
+                ! Euler-Maruyama for r_cm 
+                if (evolve_r_cm) then
+                    sdev_cm = sqrt(2 * D_cm * dt)
+                    call NormalRNGVec(numbers = disp3, n_numbers = dim) ! Mean zero and variance one
+                    r_cm = r_cm + sdev_cm * disp3                       
+                end if
+
+            end do
+
+            r_rel = u * l(1) 
+            
+        end subroutine
+
+        subroutine rotate(u, theta, N)
+            real(pr), dimension(dim), intent(inout)     :: u
+            real(pr), intent(in)                        :: theta 
+            real(pr), dimension(dim), intent(in)        :: N
+
+            ! Assumed that N is a unit vector
+            u = u * cos(theta) + cross(N,u)*sin(theta) + N * dot_product(N,u) * (1-cos(theta))
+
+        end subroutine
+
+        ! Cross product is well-defined in the 3D case so built own function assuming 3D. 
+        ! No intrinsic fortran routine I could find
+        function cross(a, b)
+            real(pr), dimension(3) :: cross
+            real(pr), dimension(3), intent(in) :: a, b
+          
+            cross(1) = a(2) * b(3) - a(3) * b(2)
+            cross(2) = a(3) * b(1) - a(1) * b(3)
+            cross(3) = a(1) * b(2) - a(2) * b(1)
+        end function cross
+          
 
 
 end module
